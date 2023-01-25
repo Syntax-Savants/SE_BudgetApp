fa450a1cd134cafc6c61501d56a7b16c8ef9cf71
Julien Goolsby <juliengoolsby@gmail.com>
2023-01-25 13:28:44 -0600

    Updated File Structure

---
+++ budget-app-frontend/src/pages/login.js
@@ -0,0 +1,20 @@
+function Login() {
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+
+                <button>Login</button>
+            </form>
+        </div>
+
+
+
+    );
+}
+
+export default Login;
