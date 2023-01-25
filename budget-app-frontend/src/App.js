6dbf2dc2515f92747c307ac8cacc8bc530d50289
Julien Goolsby <juliengoolsby@gmail.com>
2023-01-25 13:46:40 -0600

    Updated File Structure

--- budget-app-frontend/src/App.js
+++ budget-app-frontend/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import DefaultPage from './pages/DefaultPage';
-import Login from './pages/LoginPage';
 
 function App() {
-  return (<
-    Login />
+  return (<DefaultPage />
   );
 }
 
