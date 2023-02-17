85f391db03e513ee8e4b124f01bb8f1c7704738c
Connor Dalrymple <packetson2@gmail.com>
2023-02-17 13:39:26 -0600

    update file structure

--- BudgetApp/src/main/java/com/SyntaxSavants/BudgetApp/Controller/Controller.java
+++ BudgetApp/src/main/java/com/SyntaxSavants/BudgetApp/Controller/Controller.java
@@ -1,13 +1,13 @@
 package com.SyntaxSavants.BudgetApp.Controller;
-import com.SyntaxSavants.BudgetApp.Domain.CreateUserRequest;
+import com.SyntaxSavants.BudgetApp.Domain.User.CreateUserRequest;
 import com.SyntaxSavants.BudgetApp.Repository.UserRepository;
-import com.SyntaxSavants.BudgetApp.Domain.User;
+import com.SyntaxSavants.BudgetApp.Domain.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 //controller to communicate app health to web page
 @RestController
@@ -21,23 +21,5 @@ public class Controller{
         return "Endpoint met"; //postman test
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<Iterable<User>> getUser() {
-        List<User> users = userRepository.findAll();
 
-        return new ResponseEntity(users, HttpStatus.OK);
-    }
-    @PostMapping("/user")
-    public ResponseEntity<?> addNewUser(@RequestBody CreateUserRequest request) {
-        User u = new User();
-
-        u.setFirst_name(request.getFirst_name());
-        u.setLast_name(request.getLast_name());
-        u.setUsername(request.getUsername());
-        u.setPassword(request.getPassword());
-
-        userRepository.save(u);
-
-        return new ResponseEntity("it worked", HttpStatus.OK);
-    }
 }
